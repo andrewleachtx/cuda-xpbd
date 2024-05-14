f1a79842283165b6b442994aeaf397941f31a83d
KiranWells <kiranwells1008@gmail.com>
2024-05-14 13:50:56 -0500

    Inline raycast and odeboxbox

--- include/JGT-float/aabox.h
+++ include/JGT-float/aabox.h
@@ -24,6 +24,30 @@ struct aabox {
 __host__ __device__ void make_aabox(float x0, float y0, float z0, float x1,
                                     float y1, float z1, aabox *a);
 
+inline void make_aabox(float x0, float y0, float z0, float x1, float y1,
+                       float z1, aabox *a) {
+  if (x0 > x1) {
+    a->x0 = x1;
+    a->x1 = x0;
+  } else {
+    a->x0 = x0;
+    a->x1 = x1;
+  }
+  if (y0 > y1) {
+    a->y0 = y1;
+    a->y1 = y0;
+  } else {
+    a->y0 = y0;
+    a->y1 = y1;
+  }
+  if (z0 > z1) {
+    a->z0 = z1;
+    a->z1 = z0;
+  } else {
+    a->z0 = z0;
+    a->z1 = z1;
+  }
+}
 } // namespace jgt_float
 
 #endif
--- include/JGT-float/smits_mul.h
+++ include/JGT-float/smits_mul.h
@@ -20,6 +20,73 @@ namespace jgt_float {
 
 __host__ __device__ bool smits_mul(ray *r, aabox *b, float *t);
 
+inline bool smits_mul(ray *r, aabox *b, float *t) {
+  float tnear = -1e6;
+  float tfar = 1e6;
+
+  {
+    // multiply by the inverse instead of dividing
+    float t1 = (b->x0 - r->x) * r->ii;
+    float t2 = (b->x1 - r->x) * r->ii;
+
+    if (t1 > t2) {
+      float temp = t1;
+      t1 = t2;
+      t2 = temp;
+    }
+    if (t1 > tnear)
+      tnear = t1;
+    if (t2 < tfar)
+      tfar = t2;
+
+    if (tnear > tfar)
+      return false;
+    if (tfar < 0.0)
+      return false;
+  }
+  {
+    float t1 = (b->y0 - r->y) * r->ij;
+    float t2 = (b->y1 - r->y) * r->ij;
+
+    if (t1 > t2) {
+      float temp = t1;
+      t1 = t2;
+      t2 = temp;
+    }
+    if (t1 > tnear)
+      tnear = t1;
+    if (t2 < tfar)
+      tfar = t2;
+
+    if (tnear > tfar)
+      return false;
+    if (tfar < 0.0)
+      return false;
+  }
+  {
+    float t1 = (b->z0 - r->z) * r->ik;
+    float t2 = (b->z1 - r->z) * r->ik;
+
+    if (t1 > t2) {
+      float temp = t1;
+      t1 = t2;
+      t2 = temp;
+    }
+    if (t1 > tnear)
+      tnear = t1;
+    if (t2 < tfar)
+      tfar = t2;
+
+    if (tnear > tfar)
+      return false;
+    if (tfar < 0.0)
+      return false;
+  }
+
+  *t = tnear;
+  return true;
 }
+
+} // namespace jgt_float
 
 #endif
