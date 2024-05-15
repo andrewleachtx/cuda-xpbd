ee9e2234c9b54a40bf7ef6b61e6790bf4bf3baa4
KiranWells <kiranwells1008@gmail.com>
2024-05-15 14:13:28 -0500

    Change perf test script to run all scenes

--- run_perf_test.sh
+++ run_perf_test.sh
@@ -15,27 +15,54 @@ cmake --build build/$version --parallel -t performance
 echo Testing...
 echo
 
-for iter in {1..3}
+for sim_id in {1..14}
 do
-  file=output/profiling/times_$(git log --oneline | head -n 1 | awk '{ print $1 }')_${version}_3x.${iter}.csv
-
-echo -e "scene count,$version simulation time,$version total time" > $file
-echo -e "scene count\t$version simulation time\t$version total time"
-
-for sim_count in {10..82}
+  for iter in {1..3}
   do
-    sim_count=$(python3 -c 'print(int(2**('$sim_count'/5)))')
-    ./build/$version/tests/performance -m 7 -s $sim_count -v \
-      | grep "Kernel took:" \
-      | awk '{printf("'$sim_count',%s,%s\n",$4,$7)}' \
-      | tee -a $file | awk -F ',' '{print $1,"\t",$2,"\t",$3}' \
-      || (echo "Error! $sim_count"; exit 1)
+    file=output/profiling/times_$(git log --oneline | head -n 1 | awk '{ print $1 }')_${version}_sim-${sim_id}.${iter}.csv
+
+    echo -e "scene count,$version simulation time,$version total time" > $file
+    echo -e "scene count\t$version simulation time\t$version total time"
+
+    for sim_count in {10..82}
+      do
+        sim_count=$(python3 -c 'print(int(2**('$sim_count'/5)))')
+        ./build/$version/tests/performance -m $sim_id -s $sim_count \
+          | grep "Kernel took:" \
+          | awk '{printf("'$sim_count',%s,%s\n",$4,$7)}' \
+          | tee -a $file | awk -F ',' '{print $1,"\t",$2,"\t",$3}' \
+          || (echo "Error! $sim_count"; exit 1)
+      done
+
+    # run scene 7 with variations in the scenes
+    if [ $sim_id == 7 ]
+    then
+      file=output/profiling/times_$(git log --oneline | head -n 1 | awk '{ print $1 }')_${version}_sim-${sim_id}_vars.${iter}.csv
+      echo -e "scene count,$version simulation time,$version total time" > $file
+      echo -e "scene count\t$version simulation time\t$version total time"
+      for sim_count in {10..82}
+        do
+          sim_count=$(python3 -c 'print(int(2**('$sim_count'/5)))')
+          ./build/$version/tests/performance -m $sim_id -s $sim_count -v \
+            | grep "Kernel took:" \
+            | awk '{printf("'$sim_count',%s,%s\n",$4,$7)}' \
+            | tee -a $file | awk -F ',' '{print $1,"\t",$2,"\t",$3}' \
+            || (echo "Error! $sim_count"; exit 1)
+        done
+    fi
   done
+
+  cd output/profiling 
+
+  python3 data_processing.py $(git log --oneline | head -n 1 | awk '{ print $1 }') ${version} sim-${sim_id}
+
+  if [ $sim_id == 7 ]
+  then
+    python3 data_processing.py $(git log --oneline | head -n 1 | awk '{ print $1 }') ${version} sim-${sim_id}_vars
+  fi
+
+  cd ../..
 done
-
-cd output/profiling 
-
-python3 data_processing.py $(git log --oneline | head -n 1 | awk '{ print $1 }') ${version} 3x
 
 echo
 echo Done
